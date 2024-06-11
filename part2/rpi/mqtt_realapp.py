dbe8939fb5bf02216013310e26c878c1b010ef70
leekminxx <lkmm0712@gmail.com>
2024-06-11 11:27:16 +0900

    Update mqtt_realapp.py

--- part2/rpi/mqtt_realapp.py
+++ part2/rpi/mqtt_realapp.py
@@ -9,10 +9,11 @@ import board
 import time
 import datetime as dt
 import json
-
+import io
 
 red_pin = 4
 green_pin = 6
+blue_pin = 5 # RGB
 dht_pin = 18
 
 dev_id ='PKNU71'
@@ -23,14 +24,35 @@ loop_num = 0
 def onConnect(client, userdata, flags, reason_code, proferties):
     print(f'연결성공 : {reason_code}')
     client.subscribe('pknu/rcv/')
+    # RGB LED off
+    GPIO.output(red_pin, GPIO.HIGH)
+    GPIO.output(green_pin, GPIO.HIGH)
+    GPIO.output(blue_pin, GPIO.HIGH)  #LOW 하면 켜짐
 
 def onMessage(client, userdata , msg):
-    print(f'{msg.topic} + {msg.payload}')
+    # print(f'{msg.topic} + {msg.payload}')
+    # byte code -> string
+    # json ' -> "
+    value = json.loads(msg.payload.decode('utf-8').replace("'",'"'))
+    res= value['control']
+    if(res == 'warning'):
+        GPIO.output(blue_pin, GPIO.HIGH) ##off
+        GPIO.output(green_pin, GPIO.HIGH) ##off
+        GPIO.output(red_pin, GPIO.LOW) ##on
+    elif res == 'normal':
+        GPIO.output(blue_pin, GPIO.HIGH) ##off
+        GPIO.output(green_pin, GPIO.LOW) ##on
+        GPIO.output(red_pin, GPIO.HIGH) ##off
+    elif res == 'off':
+        GPIO.output(blue_pin, GPIO.HIGH) ##off
+        GPIO.output(green_pin, GPIO.HIGH) ##off
+        GPIO.output(red_pin, GPIO.HIGH) ##off
 
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(red_pin, GPIO.OUT)
 GPIO.setup(green_pin, GPIO.OUT)  #LED 켜는 것 
+GPIO.setup(blue_pin , GPIO.OUT)
 GPIO.setup(dht_pin, GPIO.IN) # 온습도값을 RPI에서 받는 것 
 dhtDevive = adafruit_dht.DHT11(board.D18) # 중요
 ## 초기화 끝
